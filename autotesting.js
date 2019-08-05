532d8399ad8e601ec6a2b36fd6b8260d6284b8cc
harrykwan <33515754+harrykwan@users.noreply.github.com>
2019-08-05 20:14:35 +0800

    Update autotesting.js

--- autotesting.js
+++ autotesting.js
@@ -3,8 +3,9 @@
 //get message frd list (expand)
 var frdlistjson = {};
 var expandtimes = 5;
-var mymessagelist = document.getElementsByClassName("_55wp _7om2 _5b6o _67ix _2ycx acw del_area async_del abb touchable _592p _25mv");
+
 function getallname(){
+	var mymessagelist = document.getElementsByClassName("_55wp _7om2 _5b6o _67ix _2ycx acw del_area async_del abb touchable _592p _25mv");
 	var oldlength = mymessagelist.length;
 	document.getElementsByClassName("title mfsm fcl")[0].click()
 	var mywaitfunc = setInterval(function(){
@@ -15,24 +16,76 @@ function getallname(){
 				getallname();
 			} else {
 				for (var j=0; j<mymessagelist.length; j++){
-					frdlistjson[mymessagelist[j].innerText.split("\n")[0]] = mymessagelist[j];
-					// console.log(mymessagelist[j].innerText.split("\n")[0])
-					return frdlistjson;
+					frdlistjson[mymessagelist[j].innerText.split("\n")[0]] = {};
+					frdlistjson[mymessagelist[j].innerText.split("\n")[0]].dom = mymessagelist[j];
+					frdlistjson[mymessagelist[j].innerText.split("\n")[0]].time = mymessagelist[j].innerText.split("\n")[2];
+					frdlistjson[mymessagelist[j].innerText.split("\n")[0]].index = j;
 				}
+				console.log(frdlistjson);
 			}
 			
 		} 
 	}, 500);
 }
-var myfrdlist = getallname();
-console.log(myfrdlist);
+getallname();
+
+
+
+
+
+
+
 
 //get into chat room
 var chosenfrd = "Cynthia Ka Yi"
-frdlistjson[chosenfrd].getElementsByTagName("a")[0].click()
+frdlistjson[chosenfrd].dom.getElementsByTagName("a")[0].click()
+
+
+
+
+
+
+
 
 //expand messages
-document.getElementsByClassName("_2so _2sq _2ss img _50cg async_throbber async_saving_visible")[0].click()
+var expandtimes = 5;
+var mychatlist = document.getElementsByClassName("voice acw");
+var mychatrecord = [];
+
+function getallmessage(){
+	if (document.getElementsByClassName("_2so _2sq _2ss img _50cg async_throbber async_saving_visible")[0])
+		document.getElementsByClassName("_2so _2sq _2ss img _50cg async_throbber async_saving_visible")[0].click()
+	var mywaitfunc = setTimeout(function(){
+		if (expandtimes>0&&document.getElementsByClassName("_2so _2sq _2ss img _50cg async_throbber async_saving_visible")[0]){
+			expandtimes--;
+			getallmessage();
+		} else {
+			for (var j=0; j<mychatlist.length; j++){
+				var tempchatlist = mychatlist[j].getElementsByClassName("_1c-b _34ee _3_5q _34eo");
+				var tempchatrecord = {};
+				tempchatrecord.name = JSON.parse(mychatlist[j].dataset.store).name
+				tempchatrecord.data = [];
+				for (var k=0; k<tempchatlist.length; k++){
+					if (tempchatlist[k]){
+						tempchatrecord.data.push(tempchatlist[k].innerText);
+					}
+				}
+				mychatrecord.push(tempchatrecord)
+			}
+			console.log(mychatrecord)
+		}
+	}, 500);
+	
+}
+getallmessage();
+
+
+
+
+
+
+
+
 
 
 //send
